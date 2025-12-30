d531db385a13d1d31a261a96c86159ff096312f1
Ole Snyders <124588267+IBimsHedebe@users.noreply.github.com>
2025-12-30 23:31:27 +0100

    Added ItemGroups

---
+++ src/main/java/megamoos/endless_seas/item/ItemGroups.java
@@ -0,0 +1,28 @@
+package megamoos.endless_seas.item;
+
+import megamoos.endless_seas.EndlessSeas;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ItemGroups {
+
+
+    public static final ItemGroup ENDLESS_SEAS_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(EndlessSeas.MOD_ID, "endless_seas_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.TRASH))
+                    .displayName(Text.translatable("itemgroup.endless_seas.endless_seas_items"))
+                    .entries((displayContext, entries) -> {
+                      entries.add(ModItems.TRASH);
+
+                    }).build());
+
+    public static void registerItemGroups(){
+        EndlessSeas.LOGGER.info("Registering Item Groups for " + EndlessSeas.MOD_ID);
+    }
+
+}
